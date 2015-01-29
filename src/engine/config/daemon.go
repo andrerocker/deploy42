689f6afa10be372622ad933d77f310158b5a08f0
andrerocker <andre.souza@gmail.com>
2015-01-29 00:53:47 -0200

    now we can load external conf files

--- src/engine/config/daemon.go
+++ src/engine/config/daemon.go
@@ -5,6 +5,7 @@ import "fmt"
 type Daemon struct {
 	Port int
 	Bind string
+	Load string
 }
 
 func (self Daemon) BindUrl() string {
--- src/engine/config/loader.go
+++ src/engine/config/loader.go
@@ -1,11 +1,29 @@
 package config
 
 import (
+	"github.com/imdario/mergo"
 	"gopkg.in/yaml.v1"
 	"io/ioutil"
+	"path/filepath"
 )
 
 func YAMLoad(configFile string) Configuration {
+	cfg := load(configFile)
+	extendPath := cfg.Daemon.Load
+
+	if extendPath != "" {
+		files, _ := filepath.Glob(extendPath)
+
+		for _, file := range files {
+			internal := load(file).Commands
+			mergo.Merge(&cfg.Commands, internal)
+		}
+	}
+
+	return cfg
+}
+
+func load(configFile string) Configuration {
 	cfg := Configuration{}
 	data, _ := ioutil.ReadFile(configFile)
 	yaml.Unmarshal(data, &cfg)
