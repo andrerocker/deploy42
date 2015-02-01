8a99afa2887cc4587121d275ea335709e3e32f8e
andrerocker <andre.souza@gmail.com>
2015-02-01 16:22:41 -0200

    add a better implementation of execute command
    - when the request dies the remote command will be killed

--- src/engine/command/executor.go
+++ src/engine/command/executor.go
@@ -7,8 +7,28 @@ import (
 )
 
 func ExecuteCommand(output io.Writer, cmd string) {
+	command, _ := composedExecuteCommand(output, cmd)
+	command.Run()
+}
+
+func basicExecuteCommand(output io.Writer, cmd string) (*exec.Cmd, error) {
 	command := exec.Command("/bin/bash", "-c", fmt.Sprintf("%s", cmd))
 	command.Stdout = output
 	command.Stderr = output
-	command.Run()
+
+	return command, nil
+}
+
+func composedExecuteCommand(requestOutput io.Writer, cmd string) (*exec.Cmd, error) {
+	command := exec.Command("/bin/bash", "-c", fmt.Sprintf("%s", cmd))
+	commandOutput, err := command.StdoutPipe()
+	go supervisor(command, requestOutput, commandOutput)
+
+	return command, err
+}
+
+func supervisor(command *exec.Cmd, output io.Writer, input io.Reader) {
+	if _, err := io.Copy(output, input); err != nil {
+		command.Process.Kill()
+	}
 }
